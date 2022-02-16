89be41af15158fd528cbe2204365c6d7b4c967df
Alcides Ojeda <eojedapilchik@gmail.com>
2022-02-16 07:55:32 +0100

    added db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+         $sql = file_get_contents(database_path() . '/seeders/transactions.sql');
+    
+         DB::unprepared($sql);
     }
 }
