1e09d4ba15d00c1ee675e1c97de4b5e5473d0dcf
Alcides Ojeda <eojedapilchik@gmail.com>
2022-02-16 09:31:33 +0100

    added validation of source

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,12 +3,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class TransactionController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'source' => [
+            'required',
+            Rule::in(['csv', 'db']),
+            ],
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json("Bad Request", 400);
+        }
+        $validated = $validator->safe()->only(['source']);
+        $source = $validated['source'];
         return Transaction::all();
     }
     
